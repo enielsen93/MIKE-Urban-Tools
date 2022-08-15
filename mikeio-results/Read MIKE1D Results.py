e0d341eecb7d89d3c3d0b9d8db566752e41ab2b7
Emil Nielsen <enielsen93@hotmail.com>
2022-08-15 07:00:40 +0200

    Added support for node depth

--- mikeio-results/Read MIKE1D Results.py
+++ mikeio-results/Read MIKE1D Results.py
@@ -4,29 +4,36 @@ import os
 import numpy as np
 
 # res1d_file = r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_Plan_027\KOM_CDS_5_sc3Base.res1d"
-res1d_file = r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_Plan_017_sc2\KOM_Plan_017_sc2_CDS_5Base.res1d"
+res1d_files = [r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_005_sc1_CDS_5Base.res1d",
+            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_017_sc2_CDS_5Base.res1d",
+            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_028_sc4_CDS_5Base.res1d",
+            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_038_sc3_CDS_5Base.res1d",
+            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_sc5_CDS_5Base.res1d"]
 
-queries = []
-res1d = Res1D(res1d_file)
-for node in res1d.data.Nodes:
-    queries.append(QueryDataNode("WaterLevel", node, validate = False))
+for res1d_file in res1d_files:
+# res1d_file = r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_Plan_017_sc2\KOM_Plan_017_sc2_CDS_5Base.res1d"
+    print(res1d_file)
+    queries = []
+    res1d = Res1D(res1d_file)
+    for node in res1d.data.Nodes:
+        queries.append(QueryDataNode("WaterLevel", node, validate = False))
 
-df = Res1D(res1d_file, queries)
-arcpy.env.overwriteOutput = True
-try:
-    output_filepath = arcpy.CreateFeatureclass_management(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""), "POINT")[0]
-    arcpy.management.AddField(output_filepath, "MUID", "TEXT")
-    arcpy.management.AddField(output_filepath, "Flood", "FLOAT", 4, 2)
-except Exception as e:
-    print(e)
-    output_filepath = os.path.join(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""))
-    with arcpy.da.UpdateCursor(output_filepath, ["MUID"]) as cursor:
-        for row in cursor:
-            cursor.deleteRow()
+    df = Res1D(res1d_file, queries)
+    arcpy.env.overwriteOutput = True
+    try:
+        output_filepath = arcpy.CreateFeatureclass_management(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""), "POINT")[0]
+        arcpy.management.AddField(output_filepath, "MUID", "TEXT")
+        arcpy.management.AddField(output_filepath, "Flood", "FLOAT", 4, 2)
+    except Exception as e:
+        print(e)
+        output_filepath = os.path.join(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""))
+        with arcpy.da.UpdateCursor(output_filepath, ["MUID"]) as cursor:
+            for row in cursor:
+                cursor.deleteRow()
 
-with arcpy.da.InsertCursor(output_filepath, ["SHAPE@", "MUID", "Flood"]) as cursor:
-    for node in df.data.Nodes:
-        muid = node.ID
-        flood_depth = np.max(df.get_node_values(muid, "WaterLevel")) - node.GroundLevel
-        if flood_depth > 0:
-            cursor.insertRow([arcpy.Point(node.XCoordinate, node.YCoordinate), muid, flood_depth])
+    with arcpy.da.InsertCursor(output_filepath, ["SHAPE@", "MUID", "Flood"]) as cursor:
+        for node in df.data.Nodes:
+            muid = node.ID
+            flood_depth = np.max(df.get_node_values(muid, "WaterLevel")) - node.GroundLevel
+            if flood_depth > 0:
+                cursor.insertRow([arcpy.Point(node.XCoordinate, node.YCoordinate), muid, flood_depth])
