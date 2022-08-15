644def4cf1c02b08a02b48017473175f6667030f
Emil Nielsen <enielsen93@hotmail.com>
2022-08-15 07:00:55 +0200

    .

--- Mike Flood - No Interface Scripts/DFS2ToTIF.py
+++ Mike Flood - No Interface Scripts/DFS2ToTIF.py
@@ -9,12 +9,14 @@ import mikeio
 import numpy as np
 import arcpy
 
-dfs2_filepath = r"C:\Offline\VOR_Status\VOR_Status_006_CDS10__CDS10_Coarse.dfs2"
+dfs2_filepath = r"C:\Offline\VOR_Status\VOR_Status_CDS5_2dCDS5.m21 - Result Files\VOR_Status_CDS20_maxA01.dfs2"
 dfs2 = mikeio.dfs2.Dfs2(dfs2_filepath)
 
 item = dfs2.items[0].name
 
 data = np.nan_to_num(dfs2.read(items = item).data[0],nan=-999)
+
+arcpy.env.overwriteOutput = True
 
 lower_left_corner = arcpy.PointGeometry(arcpy.Point(dfs2.longitude, dfs2.latitude),"GCS_WGS_1984").projectAs("ETRS_1989_UTM_Zone_32N")[0]
 raster = arcpy.NumPyArrayToRaster(np.flip(data, axis=1), lower_left_corner = lower_left_corner,
