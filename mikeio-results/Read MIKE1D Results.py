02434a5a2a1800f72062554ec983033d203f4a7c
Emil Nielsen <enielsen93@hotmail.com>
2022-09-07 08:55:37 +0200

    .

--- mikeio-results/Read MIKE1D Results.py
+++ mikeio-results/Read MIKE1D Results.py
@@ -4,11 +4,7 @@ import os
 import numpy as np
 
 # res1d_file = r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_Plan_027\KOM_CDS_5_sc3Base.res1d"
-res1d_files = [r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_005_sc1_CDS_5Base.res1d",
-            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_017_sc2_CDS_5Base.res1d",
-            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_028_sc4_CDS_5Base.res1d",
-            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_038_sc3_CDS_5Base.res1d",
-            r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\05_RESULTS\01_NETWORK\KOM_Plan_sc5_CDS_5Base.res1d"]
+res1d_files = [r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_007\KOM_007_sc1_CDS_5Base.res1d"]
 
 for res1d_file in res1d_files:
 # res1d_file = r"C:\Users\ELNN\OneDrive - Ramboll\Documents\Aarhus Vand\Kongelund og Marselistunnel\MIKE\KOM_Plan_017_sc2\KOM_Plan_017_sc2_CDS_5Base.res1d"
@@ -20,13 +16,15 @@ for res1d_file in res1d_files:
 
     df = Res1D(res1d_file, queries)
     arcpy.env.overwriteOutput = True
+    output_folder = r"C:\Papirkurv"# r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb"
+    new_filename = os.path.basename(res1d_file).replace(".res1d",".shp")
     try:
-        output_filepath = arcpy.CreateFeatureclass_management(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""), "POINT")[0]
+        output_filepath = arcpy.CreateFeatureclass_management(output_folder, new_filename, "POINT")[0]
         arcpy.management.AddField(output_filepath, "MUID", "TEXT")
         arcpy.management.AddField(output_filepath, "Flood", "FLOAT", 4, 2)
     except Exception as e:
         print(e)
-        output_filepath = os.path.join(r"C:\Users\ELNN\OneDrive - Ramboll\Documents\ArcGIS\scratch.gdb", os.path.basename(res1d_file).replace(".res1d",""))
+        output_filepath = os.path.join(output_folder, new_filename)
         with arcpy.da.UpdateCursor(output_filepath, ["MUID"]) as cursor:
             for row in cursor:
                 cursor.deleteRow()
