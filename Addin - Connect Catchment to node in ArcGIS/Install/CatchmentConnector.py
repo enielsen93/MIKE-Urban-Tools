22cf6615b2b3e639aa3fc9836d8693a9fa096448
Emil Nielsen <enielsen93@hotmail.com>
2024-10-09 12:50:17 +0200

    bugfix

--- Addin - Connect Catchment to node in ArcGIS/Install/CatchmentConnector.py
+++ Addin - Connect Catchment to node in ArcGIS/Install/CatchmentConnector.py
@@ -2,6 +2,16 @@ import arcpy
 import pythonaddins
 import numpy as np
 import os
+from scipy.spatial import KDTree
+
+def find_duplicates(strings):
+    return list(set([x for x in strings if strings.count(x) > 1]))
+
+class Node:
+    def __init__(self, muid, x, y):
+        self.muid = muid
+        self.x = x
+        self.y = y
 
 class CatchmentLayer(object):
     """Implementation for CatchmentConnector.catchmentLayer (ComboBox)"""
@@ -15,6 +25,18 @@ class CatchmentLayer(object):
     def onSelChange(self, selection):
         if selection != None:
             self.selectedLayer = selection
+            catchLayer = \
+            [layer for layer in arcpy.mapping.ListLayers(self.mxd) if layer.longName == self.selectedLayer][0]
+            msm_CatchCon = os.path.join(catchLayer.workspacePath, "msm_CatchCon")
+
+            duplicates = find_duplicates([row[0] for row in arcpy.da.SearchCursor(msm_CatchCon, ["MUID"])])
+            if duplicates:
+                # arcpy.AddMessage(["('%s')" % "', '".join(duplicates)])
+                if pythonaddins.MessageBox(
+                        ["Duplicates found in msm_CatchCon MUID: ('%s'). Continue?" % "', '".join(duplicates)],
+                        "Continue", 1) != "OK":
+                    exit()
+            print("BOB11")
         else:
             self.selectedLayer = None
     def onEditChange(self, text):
@@ -37,73 +59,72 @@ class ConnectCatchment(object):
     def __init__(self):
         self.enabled = True
         self.shape = "NONE" # Can set to "Line", "Circle" or "Rectangle" for interactive shape drawing and to activate the onLine/Polygon/Circle event sinks.
+        self.mxd = arcpy.mapping.MapDocument("CURRENT")
     def onMouseDown(self, x, y, button, shift):
         pass
     def onMouseDownMap(self, x, y, button, shift):
-        mxd = arcpy.mapping.MapDocument("CURRENT")
-        df = arcpy.mapping.ListDataFrames(mxd)[0]
-        nodeLayer = [layer for layer in arcpy.mapping.ListLayers(mxd) if layer.longName == manholeLayer.selectedLayer][0]
+        print("BOB1")
+        nodeLayer = [layer for layer in arcpy.mapping.ListLayers(self.mxd) if layer.longName == manholeLayer.selectedLayer][0]
         if arcpy.Exists(os.path.join(nodeLayer.workspacePath, "msm_Node")):
             nodeLayer = os.path.join(nodeLayer.workspacePath, "msm_Node")
-            
+
+        print("BOB2")
         nodesCount = int(arcpy.GetCount_management(nodeLayer)[0])
-        nodes = np.empty(nodesCount,dtype=object)
-        nodesX = np.empty(nodesCount)
-        nodesY = np.empty(nodesCount)
-        if len(arcpy.ListFields(nodeLayer,"MUID"))>0:
-            with arcpy.da.SearchCursor(nodeLayer, ['MUID','SHAPE@XY']) as cursor:
-                for i,row in enumerate(cursor):
-                    nodes[i] = row[0]
-                    nodesX[i] = row[1][0]
-                    nodesY[i] = row[1][1]
-        else:
-            with arcpy.da.SearchCursor(nodeLayer, ['NodeID','SHAPE@XY']) as cursor:
-                for i,row in enumerate(cursor):
-                    nodes[i] = row[0]
-                    nodesX[i] = row[1][0]
-                    nodesY[i] = row[1][1]
 
-        dist = np.sqrt(np.power(nodesX-x,2)+np.power(nodesY-y,2))
-        node = nodes[np.argmin(dist)]
+        print("BOB3")
+        node_i = manholeLayer.find_closest_node([x,y])
+        print("BOB4")
+        node = manholeLayer.nodes[node_i].muid
 
-        catchLayer = [layer for layer in arcpy.mapping.ListLayers(mxd) if layer.longName == catchmentLayer.selectedLayer][0]
-        
+        # dist = np.sqrt(np.power(nodesX-x,2)+np.power(nodesY-y,2))
+        # node = nodes[np.argmin(dist)]
+
+        print("BOB5")
+        catchLayer = [layer for layer in arcpy.mapping.ListLayers(self.mxd) if layer.longName == catchmentLayer.selectedLayer][0]
+        print("BOB6")
         mike_urban_database = os.path.dirname(arcpy.Describe(catchLayer).catalogPath).replace("\mu_Geometry", "")
         is_sqlite = True if ".sqlite" in mike_urban_database else False
 
-        def find_duplicates(strings):
-            return list(set([x for x in strings if strings.count(x) > 1]))
-
-        msm_CatchConLink = os.path.join(catchLayer.workspacePath, "msm_CatchConLink")
-        duplicates = find_duplicates([row[0] for row in arcpy.da.SearchCursor(msm_CatchConLink, ["MUID"])])
-        if duplicates:
-            # arcpy.AddMessage(["('%s')" % "', '".join(duplicates)])
-            if pythonaddins.MessageBox(["Duplicates found in msm_CatchConLink MUID: ('%s'). Continue?" % "', '".join(duplicates)], "Continue", 1) != "OK":
-                exit()
-        
+        print("BOB7")
         catchments_selected = []
         ID_fields = ["MUID","FID","OBJECTID","OID"]
+        fields = arcpy.ListFields(catchLayer)
         for ID_field in ID_fields:
-            if ID_field.lower() in [field.name.lower() for field in arcpy.ListFields(catchLayer)]:
+            if ID_field.lower() in [field.name.lower() for field in fields]:
                 break
 
+        print("BOB8")
         with arcpy.da.SearchCursor(catchLayer, [ID_field]) as cursor:
             for i,row in enumerate(cursor):
                 catchments_selected.append(str(row[0]))
-                
+
+        print("BOB9")
         if len(arcpy.ListFields(catchLayer,"NodeID"))>0:
             if pythonaddins.MessageBox("Assign %s to %s (SHP)?" % (", ".join(catchments_selected),node), "Confirm Assignment", 1) == "OK":
                 arcpy.CalculateField_management(catchLayer, "NodeID", '"%s"' % node, "VB")
         else:
+            print("BOB95")
+            if is_sqlite:
+                pass
+            else:
+                print("BOB12")
+                msm_CatchConLink = os.path.join(catchLayer.workspacePath, "msm_CatchConLink")
+                duplicates = find_duplicates([row[0] for row in arcpy.da.SearchCursor(msm_CatchConLink, ["MUID"])])
+                if duplicates:
+                    # arcpy.AddMessage(["('%s')" % "', '".join(duplicates)])
+                    if pythonaddins.MessageBox(
+                            ["Duplicates found in msm_CatchConLink MUID: ('%s'). Continue?" % "', '".join(duplicates)],
+                            "Continue", 1) != "OK":
+                        exit()
+                print("BOB13")
             if pythonaddins.MessageBox("Assign %s to %s (MDB)?" % (", ".join(catchments_selected),node), "Confirm Assignment", 1) == "OK":
                 msm_Node = os.path.join(catchLayer.workspacePath, "msm_Node")
                 ms_Catchment = os.path.join(catchLayer.workspacePath, "ms_Catchment")
                 msm_CatchCon = os.path.join(catchLayer.workspacePath, "msm_CatchCon")
 
-                msm_CatchConLink
-
                 catchments_existing = []
-                
+
+                print("BOB14")
                 if is_sqlite:
                     with arcpy.da.SearchCursor(msm_CatchCon.replace("!delete!",""), ['CatchID', 'NodeID'], where_clause = "CatchID IN ('%s')" % ("', '".join(catchments_selected))) as cursor:
                         for row in cursor:
@@ -125,6 +146,7 @@ class ConnectCatchment(object):
                     # for catchment in catchment_selected:
                         # insert_query = r"INSERT INTO msm_CatchCon (catchid, nodeid) VALUES (%s, %s)" % (catchment, node)
                     conn.close()
+                    print("BOB15")
                 else:
                     with arcpy.da.UpdateCursor(msm_CatchCon, ['CatchID', 'NodeID'], where_clause = "CatchID IN ('%s')" % ("', '".join(catchments_selected))) as cursor:
                         for row in cursor:
@@ -158,7 +180,7 @@ class ConnectCatchment(object):
                             cursor.deleteRow()
 
                     with arcpy.da.InsertCursor(msm_CatchConLink,
-                                               ["SHAPE@", "MUID", "CatchConID", "SHAPE_Length"]) as cursor:
+                                               ["SHAPE@", "CatchConID", "SHAPE_Length"]) as cursor:
                         for link_i, link in enumerate(links.keys()):
                             catchment, node = links[link]
                             catchment_coordinate = catchments_coordinates[catchment]
@@ -166,7 +188,7 @@ class ConnectCatchment(object):
                             shape = arcpy.Polyline(
                                 arcpy.Array([arcpy.Point(catchment_coordinate[0], catchment_coordinate[1]),
                                              arcpy.Point(node_coordinate[0], node_coordinate[1])]))
-                            row = [shape, link_i, link, shape.length]
+                            row = [shape, link, shape.length]
                             cursor.insertRow(row)
         pass
     def onMouseUp(self, x, y, button, shift):
@@ -201,9 +223,24 @@ class ManholeLayer(object):
         self.dropdownWidth = 'WWWWWWWWWWWWWWWWWW'
         self.width = 'WWWWWWWWWWWWWWWWWW'
         self.selectedLayer = None
+        self.nodes = {}
+        self.tree = None
     def onSelChange(self, selection):
         if selection != None:
             self.selectedLayer = selection
+            self.nodes = []
+
+            if len(arcpy.ListFields(self.selectedLayer, "MUID")) > 0:
+                with arcpy.da.SearchCursor(self.selectedLayer, ['MUID', 'SHAPE@XY']) as cursor:
+                    for i, row in enumerate(cursor):
+                        self.nodes.append(Node(row[0], row[1][0], row[1][1]))
+            else:
+                with arcpy.da.SearchCursor(nodeLayer, ['NodeID', 'SHAPE@XY']) as cursor:
+                    for i, row in enumerate(cursor):
+                        self.nodes.append(Node(row[0], row[1][0], row[1][1]))
+
+            self.nodes_kdtree = KDTree([(node.x, node.y) for node in self.nodes])
+
         else: 
             self.selectedLayer = None
     def onEditChange(self, text):
@@ -221,3 +258,7 @@ class ManholeLayer(object):
         pass
     def refresh(self):
         pass
+
+    def find_closest_node(self, coordinate):
+        distance, index = self.nodes_kdtree.query([coordinate[0], coordinate[1]])
+        return index
